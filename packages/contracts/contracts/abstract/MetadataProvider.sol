5557f77a4a60f9964661979cd44d946fbfe9d3a4
KodiKraig <cody.craig@aol.com>
2025-03-09 13:31:09 -0400

    Bump solidity version

--- packages/contracts/contracts/abstract/MetadataProvider.sol
+++ packages/contracts/contracts/abstract/MetadataProvider.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
--- packages/contracts/contracts/libs/PricingUtils.sol
+++ packages/contracts/contracts/libs/PricingUtils.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
