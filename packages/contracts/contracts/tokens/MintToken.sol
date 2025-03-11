6e55492ca4d68694e4c2702872f95119368eb7c0
KodiKraig <cody.craig@aol.com>
2025-03-11 12:04:44 -0400

    Update words

--- packages/contracts/contracts/tokens/MintToken.sol
+++ packages/contracts/contracts/tokens/MintToken.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
+
 pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -11,8 +12,8 @@ import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
  *
  * THIS IS NOT OUR OFFICIAL TOKEN.
  *
- * If we end up doing a token drop in the future, we will deploy a new token.
- * This token will be used for testing purposes only and can be minted by anyone.
+ * If we end up doing a token drop in the future, we will deploy a new contract.
+ * This token can be used for testing purposes and can be minted by anyone.
  */
 contract MintToken is ERC20, IERC165 {
     constructor() ERC20("MintToken", "MINT") {}
