9f6052d2236727d74b77852adf29260491ac4c92
KodiKraig <cody.craig@aol.com>
2025-03-25 16:37:33 -0400

    Implement org balance withdraw

--- packages/cli/src/commands/escrow/payment.ts
+++ packages/cli/src/commands/escrow/payment.ts
@@ -3,6 +3,8 @@ import { PaymentEscrow__factory } from '@product-mint/ethers-sdk';
 import { provider, signerWallet } from '../../provider';
 import { getContractAddress } from '../../contract-address';
 import { waitTx } from '../../utils/tx';
+import { ethers, formatUnits } from 'ethers';
+import { getTokenDecimals } from '../../utils/tokens';
 
 const paymentEscrow = PaymentEscrow__factory.connect(
   getContractAddress('paymentEscrow'),
@@ -40,6 +42,54 @@ export default function registerPaymentCommand(program: Command): Command {
         paymentEscrow
           .connect(signerWallet)
           .setWhitelistedToken(token, whitelisted),
+      );
+    });
+
+  payment
+    .command('orgBalance')
+    .description('Get the balance of an organization.')
+    .argument('<orgId>', 'The organization ID to get the balance of.')
+    .option(
+      '-t, --token <token>',
+      'The token address to get the balance of. Defaults to native token.',
+      ethers.ZeroAddress,
+    )
+    .action(async (orgId, options) => {
+      const { token } = options;
+
+      const balance = await paymentEscrow.orgBalances(orgId, token);
+
+      const decimals = await getTokenDecimals(token);
+      console.log(`Organization ${orgId}`);
+      console.log(`Token ${token}`);
+      console.log(`Balance ${balance} (${formatUnits(balance, decimals)})`);
+    });
+
+  payment
+    .command('withdrawOrgBalance')
+    .description('Withdraw all funds for an organization.')
+    .argument('<orgId>', 'The organization ID to withdraw funds for.')
+    .option(
+      '-t, --token <token>',
+      'The token address to get the balance of. Defaults to native token.',
+      ethers.ZeroAddress,
+    )
+    .action(async (orgId, options) => {
+      const { token } = options;
+
+      const balance = await paymentEscrow.orgBalances(orgId, token);
+
+      if (balance === 0n) {
+        console.log('No balance to withdraw.');
+        return;
+      }
+
+      console.log(`Withdrawing ${balance}...`);
+
+      await waitTx(
+        paymentEscrow
+          .connect(signerWallet)
+          .withdrawOrgBalance(orgId, token, balance),
       );
     });
 
