1795317c576e20896f3a2dfd3b37628e7b76bf09
KodiKraig <cody.craig@aol.com>
2025-03-25 20:41:46 -0400

    Update Base Mainnet contracts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -29,32 +29,32 @@ const config: HardhatUserConfig = {
     token: 'ETH',
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
-  networks: {
-    base: {
-      url: process.env.BASE_MAINNET_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    },
-    baseSepolia: {
-      url: process.env.BASE_SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: {
-      base: process.env.BASE_SCAN_API_KEY ?? '',
-      baseSepolia: process.env.BASE_SCAN_API_KEY ?? '',
-    },
-    customChains: [
-      {
-        network: 'baseSepolia',
-        chainId: 84532,
-        urls: {
-          apiURL: 'https://api-sepolia.basescan.org/api',
-          browserURL: 'https://sepolia.basescan.org',
-        },
-      },
-    ],
-  },
+  // networks: {
+  //   base: {
+  //     url: process.env.BASE_MAINNET_RPC_URL,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   baseSepolia: {
+  //     url: process.env.BASE_SEPOLIA_RPC_URL,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: {
+  //     base: process.env.BASE_SCAN_API_KEY ?? '',
+  //     baseSepolia: process.env.BASE_SCAN_API_KEY ?? '',
+  //   },
+  //   customChains: [
+  //     {
+  //       network: 'baseSepolia',
+  //       chainId: 84532,
+  //       urls: {
+  //         apiURL: 'https://api-sepolia.basescan.org/api',
+  //         browserURL: 'https://sepolia.basescan.org',
+  //       },
+  //     },
+  //   ],
+  // },
 };
 
 export default config;
