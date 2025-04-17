cf771de01c82ce669280eb7d4930236b7c97d59e
KodiKraig <cody.craig@aol.com>
2025-04-17 11:39:03 -0400

    Update contract address for renewal deployment

--- packages/contracts/ignition/modules/RenewalProcessor.ts
+++ packages/contracts/ignition/modules/RenewalProcessor.ts
@@ -1,6 +1,6 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-const CONTRACT_REGISTRY_ADDRESS = '0xcF6AA18E24B2904F4Bb007c78d2D0FAbF5961F41';
+const CONTRACT_REGISTRY_ADDRESS = '0xe495E460649166B5d49Aac122198172DC07f31e0';
 
 const RenewalProcessorModule = buildModule('RenewalProcessorModule', (m) => {
   const renewalProcessor = m.contract('RenewalProcessor', [
