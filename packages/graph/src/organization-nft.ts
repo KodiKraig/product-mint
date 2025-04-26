a56f4cb64e68d95d2e9348ebfc65d2f1e80cb6f8
KodiKraig <cody.craig@aol.com>
2025-04-26 16:46:58 -0400

    Use different entities to differentiate between the two NFTs

--- packages/graph/src/organization-nft.ts
+++ packages/graph/src/organization-nft.ts
@@ -8,47 +8,47 @@ import {
   RoleRevoked as RoleRevokedEvent,
   Transfer as TransferEvent,
   WhitelistedSet as WhitelistedSetEvent,
-} from "../generated/OrganizationNFT/OrganizationNFT"
+} from '../generated/OrganizationNFT/OrganizationNFT';
 import {
   Approval,
   ApprovalForAll,
   MetadataProviderSet,
   MintOpenSet,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
+  OrganizationNFTRoleAdminChanged,
+  OrganizationNFTRoleGranted,
+  OrganizationNFTRoleRevoked,
   Transfer,
   WhitelistedSet,
-} from "../generated/schema"
+} from '../generated/schema';
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMetadataProviderSet(
@@ -56,100 +56,100 @@ export function handleMetadataProviderSet(
 ): void {
   let entity = new MetadataProviderSet(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.previousProvider = event.params.previousProvider
-  entity.newProvider = event.params.newProvider
+  );
+  entity.previousProvider = event.params.previousProvider;
+  entity.newProvider = event.params.newProvider;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMintOpenSet(event: MintOpenSetEvent): void {
   let entity = new MintOpenSet(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.mintOpen = event.params.mintOpen
+  );
+  entity.mintOpen = event.params.mintOpen;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
+  let entity = new OrganizationNFTRoleAdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
+  );
+  entity.role = event.params.role;
+  entity.previousAdminRole = event.params.previousAdminRole;
+  entity.newAdminRole = event.params.newAdminRole;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
+  let entity = new OrganizationNFTRoleGranted(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
+  let entity = new OrganizationNFTRoleRevoked(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleWhitelistedSet(event: WhitelistedSetEvent): void {
   let entity = new WhitelistedSet(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.account = event.params.account
-  entity.isWhitelisted = event.params.isWhitelisted
+  );
+  entity.account = event.params.account;
+  entity.isWhitelisted = event.params.isWhitelisted;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
--- packages/graph/src/product-pass-nft.ts
+++ packages/graph/src/product-pass-nft.ts
@@ -5,105 +5,105 @@ import {
   OwnershipTransferStarted as OwnershipTransferStartedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent,
-} from "../generated/ProductPassNFT/ProductPassNFT"
+} from '../generated/ProductPassNFT/ProductPassNFT';
 import {
-  Approval,
-  ApprovalForAll,
-  MetadataProviderSet,
-  OwnershipTransferStarted,
-  OwnershipTransferred,
-  Transfer,
-} from "../generated/schema"
+  ProductPassNFTApproval,
+  ProductPassNFTApprovalForAll,
+  ProductPassNFTMetadataProviderSet,
+  ProductPassNFTOwnershipTransferred,
+  ProductPassNFTOwnershipTransferStarted,
+  ProductPassNFTTransfer,
+} from '../generated/schema';
 
 export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
+  let entity = new ProductPassNFTApproval(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
+  let entity = new ProductPassNFTApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMetadataProviderSet(
   event: MetadataProviderSetEvent,
 ): void {
-  let entity = new MetadataProviderSet(
+  let entity = new ProductPassNFTMetadataProviderSet(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.previousProvider = event.params.previousProvider
-  entity.newProvider = event.params.newProvider
+  );
+  entity.previousProvider = event.params.previousProvider;
+  entity.newProvider = event.params.newProvider;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferStarted(
   event: OwnershipTransferStartedEvent,
 ): void {
-  let entity = new OwnershipTransferStarted(
+  let entity = new ProductPassNFTOwnershipTransferStarted(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent,
 ): void {
-  let entity = new OwnershipTransferred(
+  let entity = new ProductPassNFTOwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
+  let entity = new ProductPassNFTTransfer(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
