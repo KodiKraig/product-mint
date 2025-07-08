2cd90a64364ecca57c982db4ff61ac3ec0e3dbed
KodiKraig <cody.craig@aol.com>
2025-07-08 16:29:21 -0400

    Add missing ProductMint contract tag

--- packages/contracts/contracts/router/MockUniswapV3Router.sol
+++ packages/contracts/contracts/router/MockUniswapV3Router.sol
@@ -9,6 +9,19 @@ import {
 
 import {ICustomUniswapV3Router} from "./ICustomUniswapV3Router.sol";
 
+/*
+ ____                 _            _   __  __ _       _   
+|  _ \ _ __ ___   __| |_   _  ___| |_|  \/  (_)_ __ | |_ 
+| |_) | '__/ _ \ / _` | | | |/ __| __| |\/| | | '_ \| __|
+|  __/| | | (_) | (_| | |_| | (__| |_| |  | | | | | | |_ 
+|_|   |_|  \___/ \__,_|\__,_|\___|\__|_|  |_|_|_| |_|\__|
+ 
+ NFT based payment system to mint products onchain with one-time payments and 
+ recurring permissionless subscriptions.
+
+ https://productmint.io
+*/
+
 /**
  * @title MockUniswapV3Router
  * @notice A mock implementation of the custom Uniswap V3 router for testing with decimal handling.
