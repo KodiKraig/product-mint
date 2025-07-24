759252edaebe893eca5bcba73999c1d8614fc3ef
KodiKraig <cody.craig@aol.com>
2025-07-24 18:28:50 -0400

    Add Ethereum Sepolia deployment

--- packages/cli/src/contract-address/index.ts
+++ packages/cli/src/contract-address/index.ts
@@ -1,10 +1,15 @@
 import devAddresses from './dev.json';
 import baseSepoliaAddresses from './base-sepolia.json';
 import baseMainnetAddresses from './base-mainnet.json';
+import ethereumSepoliaAddresses from './ethereum-sepolia.json';
 
 export type ContractAddresses = keyof typeof devAddresses;
 
-export type AddressMode = 'dev' | 'base-sepolia' | 'base-mainnet';
+export type AddressMode =
+  | 'dev'
+  | 'base-sepolia'
+  | 'base-mainnet'
+  | 'ethereum-sepolia';
 
 export function getContractAddress(contract: ContractAddresses): string {
   let address: string;
@@ -17,6 +22,8 @@ export function getContractAddress(contract: ContractAddresses): string {
     address = baseSepoliaAddresses[contract];
   } else if (mode === 'base-mainnet') {
     address = baseMainnetAddresses[contract];
+  } else if (mode === 'ethereum-sepolia') {
+    address = ethereumSepoliaAddresses[contract];
   } else {
     throw new Error(`Unsupported address mode: ${mode}`);
   }
